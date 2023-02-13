99afeae16e2c404c9dc1b08731b2ec8382c7f7e8
Joseph Muller <jtmuller5@gmail.com>
2023-02-13 10:54:09 -0500

    only need 3.0.2

--- lib/src/ui/stripe_web_view.dart
+++ lib/src/ui/stripe_web_view.dart
@@ -1,6 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+// 3.0.2
+class StripeWebView extends StatelessWidget {
+  const StripeWebView({Key? key, required this.uri, required this.returnUri}) : super(key: key);
+
+  final String uri;
+  final Uri returnUri;
+
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+        initialUrl: uri,
+        javascriptMode: JavascriptMode.unrestricted,
+        navigationDelegate: (navigation) {
+          final uri = Uri.parse(navigation.url);
+          if (uri.scheme == returnUri.scheme &&
+              uri.host == returnUri.host &&
+              uri.queryParameters['requestId'] == returnUri.queryParameters['requestId']) {
+            Navigator.pop(context, true);
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        });
+  }
+}
+
+// 4.0.2
+/*
 // Import for Android features.
 import 'package:webview_flutter_android/webview_flutter_android.dart';
 
@@ -90,3 +117,4 @@ class _StripeWebViewState extends State<StripeWebView> {
     return WebViewWidget(controller: _controller);
   }
 }
+*/
